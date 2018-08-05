7944e1b022b36267de12a30d7480f0565eed2e61
ankur <ankur.bansal@gmail.com>
2018-08-05 14:27:44 +0530

    Added methods to demo /GET /POST request

--- src/main/java/com/example/springbootrestexample/controller/EmployeeDetailsController.java
+++ src/main/java/com/example/springbootrestexample/controller/EmployeeDetailsController.java
@@ -7,12 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springbootrestexample.model.Employee;
 import com.example.springbootrestexample.service.impl.EmployeeServiceImpl;
 
 @RestController
+@RequestMapping("/spring-rest-webapp")
 public class EmployeeDetailsController {
 	
 	@Autowired
@@ -20,16 +24,34 @@ public class EmployeeDetailsController {
 	
 	@GetMapping(path="/employee/{id}")
 	public Employee findEmployeeDetails(@PathVariable int id){
-		return null;
+		return employeeService.findEmployeeDetails(id);
 	
 	}
 	
 	@GetMapping(path="/employee/findall")
-	public ResponseEntity<?> findAllEmployeeDetails(){
+	public ResponseEntity findAllEmployeeDetails(){
 		
 		List<Employee> empList=new ArrayList<Employee>();
 		
 		empList=employeeService.findAllEmployeeDetails();
+		
+		return ResponseEntity.ok().body(empList);
+		
+	}
+	
+	@PostMapping(path="/employee")
+	public ResponseEntity<?> saveEmployeeDetails(@RequestBody Employee employee){
+		
+		List<Employee> empList=employeeService.saveEmployeeDetails(employee);
+		
+		return ResponseEntity.ok().body(empList);
+		
+	}
+	
+	@GetMapping(path="/employee/remove/{id}")
+	public ResponseEntity<?> removeEmployee(@PathVariable String id){
+		
+		List<Employee> empList=employeeService.deleteEmployee(Integer.valueOf(id));
 		
 		return ResponseEntity.ok().body(empList);
 		
--- src/main/java/com/example/springbootrestexample/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/example/springbootrestexample/service/impl/EmployeeServiceImpl.java
@@ -1,21 +1,61 @@
 package com.example.springbootrestexample.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import com.example.springbootrestexample.model.Employee;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
+import com.example.springbootrestexample.model.Employee;
+@Service
 public class EmployeeServiceImpl {
 
+	List<Employee> empList=null;
+
 	public List<Employee> findAllEmployeeDetails() {
-		return null;
+
+		Employee e1 = new Employee(1, "James", "IT");
+		Employee e2 = new Employee(2, "Charles", "HR");
+		empList=new ArrayList<Employee>();
+		empList.add(e1);
+		empList.add(e2);
+
+		return empList;
 	}
 	
-	public Employee findEmployeeDetails() {
+	public Employee findEmployeeDetails(int empId ) {
+		
+		for (Employee employee : empList) {
+			if(empId==employee.getEmpId()){
+				return employee;
+			}
+		}
 		return null;
 	}
 
 	
-	public Employee saveEmployeeDetails() {
-		return null;
+	public List<Employee> saveEmployeeDetails(Employee employee) {
+		 if(CollectionUtils.isEmpty(empList)){
+			 employee.setEmpId(empList.size()+1);
+			 empList.add(employee);
+		}else if (!empList.contains(employee)) {
+			employee.setEmpId(empList.size()+1);
+			empList.add(employee);
+		}
+
+		return empList;
+
+	}
+	
+	public List<Employee> deleteEmployee(int id) {
+		for (Employee employee : empList) {
+			if(id==employee.getEmpId()){
+				empList.remove(employee);
+				return empList;
+			}
+		}
+
+		return empList;
+
 	}
 }
